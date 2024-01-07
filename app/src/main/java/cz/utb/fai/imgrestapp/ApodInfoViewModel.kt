8ed1f9e3262bc1549b21ce2faa3fbb18a8dce8a2
ruby24 <jruba@email.cz>
2024-01-07 01:15:49 +0100

    ApodInfoViewModel.kt - provides apodInfoValue property - provides getApodInfo()

--- app/src/main/java/cz/utb/fai/imgrestapp/ApodInfoViewModel.kt
+++ app/src/main/java/cz/utb/fai/imgrestapp/ApodInfoViewModel.kt
@@ -1,11 +1,27 @@
 package cz.utb.fai.imgrestapp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cz.utb.fai.imgrestapp.api.ApodRequestDto
+import cz.utb.fai.imgrestapp.api.ApodResponseDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ApodInfoViewModel(
     private val repository: Repository
 ) : ViewModel() {
 
+    private val _apodinfoValue = MutableLiveData<ApodResponseDto>()
+    val apodinfoValue: LiveData<ApodResponseDto> = _apodinfoValue
+    fun getApodInfo(requestDto: ApodRequestDto) {
+
+        viewModelScope.launch(Dispatchers.IO) {
+           val result =  repository.getApodResponse(requestDto)
+            _apodinfoValue.postValue(result)
+        }
+    }
 
 
 }
